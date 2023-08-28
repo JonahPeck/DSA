b055da06af83a1ad03ce3910d696512e33a048ce
JonahPeck <PeckJonahC@gmail.com>
2023-08-27 20:15:35 -0400

    linked list constuctor

--- main.py
+++ main.py
@@ -121,20 +121,41 @@
 
 #A noe is the value as well as the pointer. Essentially a key value pair of "4" and "None"
 
-head = {
-    "value": 11,
-    "next": {
-        "value": 3,
-        "next": {
-            "value": 23,
-            "next": {
-                "value": 7,
-                "next": None
-                }
-            }
-        }
-    }
-print(head['next']['next']['value'])
+# head = {
+#     "value": 11,
+#     "next": {
+#         "value": 3,
+#         "next": {
+#             "value": 23,
+#             "next": {
+#                 "value": 7,
+#                 "next": None
+#                 }
+#             }
+#         }
+#     }
+# print(head['next']['next']['value'])
 
-#This will only run with a linked list
-print(my_linked_list.head.next.next.value)
+# #This will only run with a linked list
+# print(my_linked_list.head.next.next.value)
+
+#constructors
+# A Constructor will create a new node 
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+my_linked_list = LinkedList(4)
+print(my_linked_list.head.value)
+
+#all this will do is create a constructor
+# class Node:
+#     def __init__(self, value):
