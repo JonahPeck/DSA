b7e3c7d28519871278d6d5586ca765829b1133d4
JonahPeck <PeckJonahC@gmail.com>
2023-09-05 14:39:36 -0400

    finished with DLL

--- DoublyLinkedList/DLL:Notes.py
+++ DoublyLinkedList/DLL:Notes.py
@@ -115,6 +115,23 @@ class DoublyLinkedList:
         after.prev = new_node
         self.length += 1
         return True
+    
+    def remove(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        
+        temp = self.get(index)
+        temp.prev.next = temp.next
+        temp.next.prev = temp.prev
+        temp.prev = None
+        temp.next = None
+        self.length -= 1
+        return temp
+
 
 
 
@@ -128,7 +145,7 @@ my_doubly_linked_list.append(3)
 my_doubly_linked_list.append(23)
 my_doubly_linked_list.append(7)
 
-my_doubly_linked_list.insert(2,4)
+my_doubly_linked_list.remove(2)
 
 my_doubly_linked_list.print_list()
 
