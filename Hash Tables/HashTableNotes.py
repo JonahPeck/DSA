983d391cb5cb3365d2307658aec66790de5b8db5
JonahPeck <PeckJonahC@gmail.com>
2023-09-15 11:51:40 -0400

    collisions

---
+++ Hash Tables/HashTableNotes.py
@@ -0,0 +1,9 @@
+#Hash Tables - dictionaries are made up of a key value pair
+#hash tables work through a hash function works by performing a hash on a key and run it through a function and it returns an address for the key value pair
+#this is how a dictionary is stored
+#Hashes are one way - you can only insert into a hash and recieve an address not vice versa
+#Hashes are deterministic - every time a key is put in you will expect to get the same output
+
+#collisions - happens when a key value pair is put in a place where another key value pair already exists
+#separate chaining - putting 
+#linear probing - form of open addressing
