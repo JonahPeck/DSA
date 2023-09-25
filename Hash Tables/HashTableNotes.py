e2aa10e2556c01c4196ee66ee5c5e0cd41d607dd
JonahPeck <PeckJonahC@gmail.com>
2023-09-25 17:04:15 -0400

    get in hash"

--- Hash Tables/HashTableNotes.py
+++ Hash Tables/HashTableNotes.py
@@ -21,10 +21,31 @@ class HashTable:
         for letter in key:
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
         return my_hash
+#this line of code above creates the random address hash method by which key value pairs will receive an address
 #ord is short for ordinal - grabs the ASCII numerical value for each letter
     def print_table(self):
         for i, val in enumerate(self.data_map):
             print(i, ": ", val)
 
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get_item(self,key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+
 my_hash_table = HashTable()
-my_hash_table.print_table()
+my_hash_table.set_item('bolts', 300)
+my_hash_table.set_item('washers', 1500)
+my_hash_table.set_item('nuts', 700)
+
+print(my_hash_table.get_item('nuts'))
+print(my_hash_table.get_item('lumber'))
+
